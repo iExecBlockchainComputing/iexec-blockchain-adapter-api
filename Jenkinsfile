f6cdfcc6b57c80ca5e9898189c0b385f63f12959
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2021-03-18 11:21:49 +0100

    Init project

---
+++ Jenkinsfile
@@ -0,0 +1,4 @@
+//Readme @ http://gitlab.iex.ec:30000/iexec/jenkins-library
+
+@Library('jenkins-library@master') _
+buildSimpleDocker(imageprivacy: 'public')
---
+++ src/main/java/com/iexec/chain/blockchainclient/StatusController.java
@@ -0,0 +1,20 @@
+package com.iexec.chain.blockchainclient;
+
+import com.iexec.common.replicate.ReplicateStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+    @GetMapping
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok(Instant.now().toString());
+    }
+
+}
