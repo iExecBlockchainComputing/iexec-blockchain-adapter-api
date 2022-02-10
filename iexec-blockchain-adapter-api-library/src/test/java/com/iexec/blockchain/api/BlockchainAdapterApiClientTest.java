cb9634df363b4f8a9c72f6c6db0d4520e1faf1af
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-02-10 16:49:51 +0100

    Add BlockchainAdapterApiClient instantiation test

---
+++ iexec-blockchain-adapter-api-library/src/test/java/com/iexec/blockchain/api/BlockchainAdapterApiClientTest.java
@@ -0,0 +1,15 @@
+package com.iexec.blockchain.api;
+
+import feign.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BlockchainAdapterApiClientTest {
+
+    @Test
+    void instantiationTest() {
+        Assertions.assertNotNull(BlockchainAdapterApiClientBuilder
+                .getInstance(Logger.Level.FULL, "localhost", "username", "password"));
+    }
+
+}
