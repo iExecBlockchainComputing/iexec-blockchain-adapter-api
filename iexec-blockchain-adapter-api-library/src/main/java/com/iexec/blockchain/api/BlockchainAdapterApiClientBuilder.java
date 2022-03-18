4876b3c6c1cabf060d6a70864fba852f0e134590
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-03-18 18:00:33 +0100

    Use iexec-common methods to create both authenticated and unauthenticated feign clients

--- iexec-blockchain-adapter-api-library/src/main/java/com/iexec/blockchain/api/BlockchainAdapterApiClientBuilder.java
+++ iexec-blockchain-adapter-api-library/src/main/java/com/iexec/blockchain/api/BlockchainAdapterApiClientBuilder.java
@@ -2,21 +2,36 @@ package com.iexec.blockchain.api;
 
 import com.iexec.common.utils.FeignBuilder;
 import feign.Logger;
-import feign.auth.BasicAuthRequestInterceptor;
 
+/**
+ * Creates Feign client instances to query REST endpoints described in {@link BlockchainAdapterApiClient}.
+ */
 public class BlockchainAdapterApiClientBuilder {
 
     private BlockchainAdapterApiClientBuilder() {}
 
-    public static BlockchainAdapterApiClient getInstance(Logger.Level logLevel,
-                                                         String url, String username, String password) {
-        BasicAuthRequestInterceptor requestInterceptor =
-                new BasicAuthRequestInterceptor(
-                        username,
-                        password
-                );
+    /**
+     * Create an unauthenticated feign client to query apis described in {@link BlockchainAdapterApiClient}.
+     * @param logLevel Feign logging level to configure.
+     * @param url Url targeted by the client.
+     * @return Feign client for {@link BlockchainAdapterApiClient} apis.
+     */
+    public static BlockchainAdapterApiClient getInstance(Logger.Level logLevel, String url) {
         return FeignBuilder.createBuilder(logLevel)
-                .requestInterceptor(requestInterceptor)
+                .target(BlockchainAdapterApiClient.class, url);
+    }
+
+    /**
+     * Create an authenticated feign client to query apis described in {@link BlockchainAdapterApiClient}.
+     * @param logLevel Feign logging level to configure.
+     * @param url Url targeted by the client.
+     * @param username Basic authentication username.
+     * @param password Basic authentication password.
+     * @return Feign client with basic authentication for {@link BlockchainAdapterApiClient} apis.
+     */
+    public static BlockchainAdapterApiClient getInstance(Logger.Level logLevel, String url,
+                                                         String username, String password) {
+        return FeignBuilder.createBuilder(logLevel, username, password)
                 .target(BlockchainAdapterApiClient.class, url);
     }
 
