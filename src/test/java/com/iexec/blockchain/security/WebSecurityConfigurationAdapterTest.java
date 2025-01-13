e4715632b74ca2376eeedb339c5a1c4ad0699a61
nabil-Tounarti <nabil.tounarti0@gmail.com>
2025-01-13 15:00:07 +0100

    Refactor WebSecurityConfigurationAdapterTest

--- src/test/java/com/iexec/blockchain/security/WebSecurityConfigurationAdapterTest.java
+++ src/test/java/com/iexec/blockchain/security/WebSecurityConfigurationAdapterTest.java
@@ -17,8 +17,6 @@
 package com.iexec.blockchain.security;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,7 +28,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
 class WebSecurityConfigurationAdapterTest {
 
     @Autowired
