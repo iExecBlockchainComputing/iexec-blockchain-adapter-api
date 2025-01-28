46e25266cb67aeffc4fa4fe7692e7f3a66ecc62d
Jeremy Bernard <jeremy.bernard@iex.ec>
2025-01-28 11:03:36 +0100

    Update licences

--- src/main/java/com/iexec/blockchain/security/WebSecurityConfigurationAdapter.java
+++ src/main/java/com/iexec/blockchain/security/WebSecurityConfigurationAdapter.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2021-2025 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.iexec.blockchain.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,8 +36,8 @@ public class WebSecurityConfigurationAdapter {
                     (or PUT/DELETE/PATCH) request with a valid CSRF token
                     Will eventually activate it later.
                 */
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.and())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(SecurityConfigurerAdapter::and)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui.html",
